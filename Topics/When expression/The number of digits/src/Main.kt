13c7b90128fac642a53adfc966b31dac31ee6b31
ka <insideka01@gmail.com>
2022-08-04 15:10:12 +0900

    08.04

---
+++ Topics/When expression/The number of digits/src/Main.kt
@@ -0,0 +1,10 @@
+fun main() {
+    println(
+        when (readln().length.toString()) {
+            "1" -> '1'
+            "2" -> '2'
+            "3" -> '3'
+            else -> '4'
+        }
+    )
+}
