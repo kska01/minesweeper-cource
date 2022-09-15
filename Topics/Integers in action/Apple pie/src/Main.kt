4f72602932f88312a8f0dfb97a249712f0551312
ka <insideka01@gmail.com>
2022-09-15 16:22:51 +0900

    09.15

---
+++ Topics/Integers in action/Apple pie/src/Main.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val yourApples = 5U
+    val friendsApples = 7U
+    println(yourApples + friendsApples) 
+
+    // do not change the code below
+    println("${yourApples::class.simpleName}")
+    println("${friendsApples::class.simpleName}")
+}
