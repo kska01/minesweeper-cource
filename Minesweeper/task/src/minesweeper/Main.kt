48aff36ddb3baf8e4e0e08d717ac6bd81f05e340
ka <insideka01@gmail.com>
2022-09-20 14:19:23 +0900

    09.20

--- Minesweeper/task/src/minesweeper/Main.kt
+++ Minesweeper/task/src/minesweeper/Main.kt
@@ -1,17 +1,25 @@
 package minesweeper
 
 fun main() {
-    print(
-        """
-            .x.......
-            ....xx...
-            ..x......
-            x........
-            ...x..x..
-            ........x
-            .........
-            .....x...
-            .......x.
-        """.trimIndent()
-    )
+    print("How many mines do you want on the field? > ")
+    val numberOfMines = readln().toInt()
+    val field = MutableList(9) { MutableList(9) {'.'} }
+
+    repeat(numberOfMines) {
+        var row = (0..8).random()
+        var col = (0..8).random()
+
+        while (field[row][col] == 'X') {
+            row = (0..8).random()
+            col = (0..8).random()
+        }
+
+        field[row][col] = 'X'
+    }
+    for (i in 0..8) {
+        for (j in 0..8) {
+            print(field[i][j])
+        }
+        println()
+    }
 }
