5174d2f5ce0387df74272475cde54d01898083b6
ka <insideka01@gmail.com>
2022-11-23 14:50:53 +0900

    11.23

---
+++ Topics/When expression/Strings as objects/src/Main.kt
@@ -0,0 +1,15 @@
+fun main() {
+    val input = readLine()!!
+
+    if (input.isEmpty()) {
+        println()
+        return
+    }
+    println(
+        when (input.first()) {
+            'i' -> input.drop(1).toInt() + 1
+            's' -> input.drop(1).reversed()
+            else -> input
+        }
+    )
+}
